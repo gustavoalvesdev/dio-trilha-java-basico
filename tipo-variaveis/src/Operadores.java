3db3046e8f7b3224858c9988a397638046011c8e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-07-02 23:11:39 -0300

    Operadores - 3

--- tipo-variaveis/src/Operadores.java
+++ tipo-variaveis/src/Operadores.java
@@ -1,26 +1,15 @@
 public class Operadores {
     public static void main(String[] args) {
-        String concatenacao = "?";
-
-        concatenacao = 1 + 1 + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
         
-        System.out.println(concatenacao);
+        int numero = 5;
+        
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
 
     }
 }
